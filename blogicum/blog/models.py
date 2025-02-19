db3fdd5b722771dd0b9d932a9fb00ee844d12fdd
nikiarohK <khoraikin3@gmail.com>
2025-02-19 21:18:40 +0300

    finish

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -12,9 +12,8 @@ class Category(models.Model):
     slug = models.SlugField(
         unique=True,
         verbose_name="Идентификатор",
-        help_text="Идентификатор страницы для URL; разрешены символы латиницы,\
-            цифры, дефис и подчёркивание.",
-    )
+        help_text="Идентификатор страницы для URL; "
+        "разрешены символы латиницы, цифры, дефис и подчёркивание.")
     is_published = models.BooleanField(
         default=True,
         verbose_name="Опубликовано",
@@ -48,8 +47,9 @@ class Post(models.Model):
     text = models.TextField(verbose_name="Текст")
     pub_date = models.DateTimeField(
         verbose_name="Дата и время публикации",
-        help_text="Если установить дату и время в\
-            будущем — можно делать отложенные публикации.",
+        help_text=(
+            "Если установить дату и время в будущем — "
+            "можно делать отложенные публикации.")
     )
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, verbose_name="Автор публикации"
